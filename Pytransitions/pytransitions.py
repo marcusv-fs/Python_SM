505fac6c61e9e33a63552d699a6b48963ce157d6
marcusv_fs <marcusv_fs>
2024-02-14 13:17:28 -0300

    Correções na Pytransitions e na PSM. Adição de guardas na PSM

--- Pytransitions/pytransitions.py
+++ Pytransitions/pytransitions.py
@@ -1,36 +1,22 @@
-# Criar um semáforo com 3 estados
-# 1 - Vermelho, 2 - Amarelo, 3 - Verde
-# Verde -> Amarelo -> Vermelho -> Verde
-# Ele deve demorar 10s no vermelho, 2s no amarelo e 5s no verde.
-# Se receber um sinal de um botão enquanto está no verde, 
-# ele deve mudar de estado para o amarelo imediatamente
-
-import random
-import sys
-import time
+import random, sys, time
 from transitions.extensions import GraphMachine
 
 class Semaforo(GraphMachine):
+####################### States Declaration #######################   
     states = ['Initial','Vermelho','Amarelo','Verde','Final']
 
+####################### Transitions Statement  #######################  
     transitions = [
         {'trigger': 'start', 'source': 'Initial', 'dest': 'Vermelho'},
         {'trigger': 'tp_Verde', 'source': 'Vermelho', 'dest': 'Verde', 'conditions': 'c_EnergiaSuficiente'},
         {'trigger': 'tp_Vermelho', 'source': 'Amarelo', 'dest': 'Vermelho', 'conditions': 'c_EnergiaSuficiente', 'unless': 'c_MuitosDefeitos'},
-        {'trigger': 'tp_Defeito', 'source': 'Amarelo', 'dest': 'Amarelo', 'before': 'b_Erro', 'unless': ['c_EnergiaSuficiente', 'c_MuitosDefeitos']},
+        {'trigger': 'tp_Defeito', 'source': 'Amarelo', 'dest': 'Amarelo', 'before': 'b_tp_Defeito', 'unless': ['c_EnergiaSuficiente', 'c_MuitosDefeitos']},
         {'trigger': 'tp_Amarelo', 'source': 'Verde', 'dest': 'Amarelo', 'conditions': 'c_EnergiaSuficiente'},
-        {'trigger': 'tp_Emergencia', 'source': ['Verde', 'Vermelho'], 'dest': 'Amarelo', 'before':'b_Aviso', 'unless': 'c_EnergiaSuficiente'},
+        {'trigger': 'tp_Emergencia', 'source': ['Verde', 'Vermelho'], 'dest': 'Amarelo', 'before':'b_tp_Emergencia', 'unless': 'c_EnergiaSuficiente'},
         {'trigger': 'end', 'source': 'Amarelo', 'dest': 'Final', 'conditions': 'c_MuitosDefeitos'}
      ]
     
-####################### Transition Conditions ####################### 
-    def c_EnergiaSuficiente(self):
-        return self.Energia >= 10
-    
-    def c_MuitosDefeitos(self):
-        return self.Defeitos >= 3
-
-####################### Init and assist functions ####################### 
+####################### Init and util Functions ####################### 
     def __init__(self):
         super().__init__(name="Semaforo", states=Semaforo.states, transitions=Semaforo.transitions, initial='Initial', show_conditions=True, show_state_attributes=True)
         self.Energia = 100
@@ -43,13 +29,23 @@ class Semaforo(GraphMachine):
         while tempo_atual - tempo_inicial < segundos and self.Energia > 10:
             tempo_atual = time.time()
 
+####################### Transition Conditions ####################### 
+    def c_EnergiaSuficiente(self):
+        return self.Energia >= 10
+    
+    def c_MuitosDefeitos(self):
+        return self.Defeitos > 3
+
 ####################### Before Transitions ####################### 
-    def b_Aviso(self):
+    def b_tp_Emergencia(self):
         print('Emergência!!! Energia Baixa. Mudando para o Amarelo')
     
-    def b_Erro(self):
+    def b_tp_Defeito(self):
         print('A energia está muito baixa. O semáforo está com defeito. Energia atual: ' + str(self.Energia))
-        
+        recarga = random.randint(0,100)
+        self.Energia = self.Energia + recarga
+        print("Foram recarregados " + str(recarga) + " de energia agora")
+
 ####################### On_enter States ####################### 
     def on_enter_Final(self):
         print("Fim de execução")
@@ -59,22 +55,21 @@ class Semaforo(GraphMachine):
         print('Estou no Vermelho. Energia atual: ' + str(self.Energia))
         self.temporizador(0.1)
 
-    def on_enter_Amarelo(self): #####Falta arrumar a energia aqui do amarelo
+    def on_enter_Amarelo(self):
         if(self.Energia < 10):
             print('Energia atual: ' + str(self.Energia))
             self.Defeitos = self.Defeitos + 1
             print("Defeitos: " + str(self.Defeitos))
-            if(self.Defeitos >= 3):
+            if(self.Defeitos > 3):
                 self.end()
-            self.Energia = self.Energia + random.randint(0,100)
             self.tp_Defeito()        
-        print('Estou no Amarelo. Energia atual: ' + str(self.Energia))
-        self.temporizador(0.1)
+        else:        
+            print('Estou no Amarelo. Energia atual: ' + str(self.Energia))
+            self.temporizador(0.1)
 
     def on_enter_Verde(self):
         print('Estou no Verde. Energia atual: ' + str(self.Energia))
         self.temporizador(0.1)
-
 
 ####################### Instantiating all objects #######################  
 machine = Semaforo()
--- python-statemachine/psm.py
+++ python-statemachine/psm.py
@@ -1,34 +1,55 @@
-import random, time
-import sys
+import random, time, sys
 from statemachine import State, StateMachine
 
-class TrafficLightIsolatedTransitions(StateMachine):
-    Energia = 100
-    Defeitos = 0
-    "A traffic light machine"
+class Python_statemachine(StateMachine):
+####################### States Declaration #######################   
     Initial = State(initial=True)
     Verde = State()
     Amarelo = State()
     Vermelho= State()
     Final = State()
-    
-    tp_Amarelo = Verde.to(Amarelo)
-    tp_Vermelho = Amarelo.to(Vermelho)
-    tp_Verde = Vermelho.to(Verde)
+
+####################### Transitions Statement  #######################  
+    tp_Amarelo = Verde.to(Amarelo, cond="c_EnergiaSuficiente")
+    tp_Vermelho = Amarelo.to(Vermelho, cond="c_EnergiaSuficiente", unless="c_MuitosDefeitos")
+    tp_Verde = Vermelho.to(Verde, cond="c_EnergiaSuficiente")
     start = Initial.to(Vermelho)
-    end = Amarelo.to(Final)
+    end = Amarelo.to(Final, cond="c_MuitosDefeitos")
 
-    tp_Emergencia = Verde.to(Amarelo) | Vermelho.to(Amarelo)
-    tp_Normal = tp_Verde | tp_Vermelho | tp_Amarelo
+    tp_Emergencia = Verde.to(Amarelo, unless="c_EnergiaSuficiente") | Vermelho.to(Amarelo, unless="c_EnergiaSuficiente")
+    # tp_Normal = tp_Verde | tp_Vermelho | tp_Amarelo # Descomente esse e a condição no while true para usar o modo alternativo
 
-    tp_Defeito = Amarelo.to(Amarelo)
+    tp_Defeito = Amarelo.to(Amarelo, unless=["c_MuitosDefeitos", "c_EnergiaSuficiente"])
 
-    def before_tp_Emergência(self):
+####################### Init and util Functions ####################### 
+    Energia = 100
+    Defeitos = 0
+    def temporizador(self, segundos):
+        tempo_inicial = time.time()
+        tempo_atual = tempo_inicial
+        self.Energia = self.Energia - 10
+        while tempo_atual - tempo_inicial < segundos and self.Energia > 10:
+            tempo_atual = time.time()
+            
+####################### Transition Conditions ####################### 
+    def c_EnergiaSuficiente(self):
+        return self.Energia >= 10
+    
+    def c_MuitosDefeitos(self):
+        return self.Defeitos > 3
+
+####################### Before Transitions ####################### 
+    def before_tp_Emergencia(self):
         print('Emergência!!! Energia Baixa. Mudando para o Amarelo')
     
     def before_tp_Defeito(self):
         print('A energia está muito baixa. O semáforo está com defeito. Energia atual: ' + str(self.Energia))
+        recarga = random.randint(0,100)
+        self.Energia = self.Energia + recarga
+        print("Foram recarregados " + str(recarga) + " de energia agora")
 
+
+####################### On_enter States ####################### 
     def on_enter_Vermelho(self):
         print('Estou no Vermelho. Energia atual: ' + str(self.Energia))
         self.temporizador(0.1)
@@ -37,37 +58,31 @@ class TrafficLightIsolatedTransitions(StateMachine):
         print("Fim de execução")
         sys.exit()
 
-    def on_enter_Amarelo(self):
+    def on_enter_Amarelo(self): 
         if(self.Energia < 10):
             print('Energia atual: ' + str(self.Energia))
             self.Defeitos = self.Defeitos + 1
             print("Defeitos: " + str(self.Defeitos))
-            if(self.Defeitos >= 3):
+            if(self.Defeitos > 3):
                 self.end()
-            self.Energia = self.Energia + random.randint(0,100)
-            self.tp_Defeito()        
-        print('Estou no Amarelo. Energia atual: ' + str(self.Energia))
-        self.temporizador(0.1)
+            self.tp_Defeito()   
+        else:        
+            print('Estou no Amarelo. Energia atual: ' + str(self.Energia))
+            self.temporizador(0.1)
  
     def on_enter_Verde(self):
         print('Estou no Verde. Energia atual: ' + str(self.Energia))
         self.temporizador(0.1)
-
-    def temporizador(self, segundos):
-        tempo_inicial = time.time()
-        tempo_atual = tempo_inicial
-        self.Energia = self.Energia - 10
-        while tempo_atual - tempo_inicial < segundos and self.Energia > 10:
-            tempo_atual = time.time()
         
+####################### Instantiating all objects #######################  
+machine = Python_statemachine()
 
-machine = TrafficLightIsolatedTransitions()
-
-img_path = "python-statemachine/psm3.png"
+####################### Draw State Machine ####################### 
+img_path = "python-statemachine/psm.png"
 machine._graph().write_png(img_path)
 
+####################### Running the State Machine ####################### 
 machine.start()
-
 while True:
     if machine.Energia < 10:
         if machine.current_state.id != 'Amarelo':
