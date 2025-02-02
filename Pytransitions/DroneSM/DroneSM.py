e9b564aead027ea40ca344da7e2565da8aa0b43b
marcusv-fs <marcusv_fs@hotmail.com>
2025-02-02 16:20:09 -0300

    Adding trace and log files.

--- Pytransitions/DroneSM/DroneSM.py
+++ Pytransitions/DroneSM/DroneSM.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from __future__ import print_function
+import random, time
 import time
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 from transitions.extensions import GraphMachine
@@ -27,7 +26,7 @@ class DroneSM(GraphMachine):
 
         #connection_string = "tcp:127.0.0.1:5760"
         self.connection_string = "127.0.0.1:14550"
-        self.aTargetAltitude = 0
+        self.heightTg = 0
         self.sleep_time = 0
         self.start = False
 
@@ -57,6 +56,8 @@ class DroneSM(GraphMachine):
     def  cond_Wait_TurnOn(self):
         if self.start:
             file.write("\ncond_Wait_TurnOn = True")
+            text = " -> DroneSM::start.in." + str(self.heightTg)
+            trace.write(text) 
             return True
         file.write("\ncond_Wait_TurnOn = False")
         return False
@@ -69,7 +70,7 @@ class DroneSM(GraphMachine):
         return False
 
     def cond_TakeOff_Mission(self):
-        if (self.vehicle.location.global_relative_frame.alt >= self.aTargetAltitude * 0.95):
+        if (self.vehicle.location.global_relative_frame.alt >= self.heightTg * 0.95):
             file.write("\ncond_TakeOff_Mission = True")
             return  True
         file.write("\ncond_TakeOff_Mission = False")
@@ -78,6 +79,7 @@ class DroneSM(GraphMachine):
 ####################### Before Transitions ####################### 
     def before_Mission_Final(self):
         file.write("\nbefore_Mission_Final")
+        file.write("\nReturning to Launch")
         print("Returning to Launch")
         self.vehicle.mode = VehicleMode("RTL")
         time.sleep(self.sleep_time)
@@ -96,8 +98,8 @@ class DroneSM(GraphMachine):
         while line != "":
             line = configFile.readline()
             if lineCont == 0:
-                print("aTargetAltitude: ", line)
-                self.aTargetAltitude = float(line)
+                print("heightTg: ", line)
+                self.heightTg = float(line)
             if lineCont == 1:
                 print("sleep_time: ", line)
                 self.sleep_time = float(line)
@@ -124,7 +126,6 @@ class DroneSM(GraphMachine):
     def on_enter_Wait(self):
         file.write("\non_enter_Wait")
         print("Waiting")
-        self.start = True
         # Don't try to arm until autopilot is ready
 
     def on_enter_TurnOn(self):
@@ -136,34 +137,53 @@ class DroneSM(GraphMachine):
 
     def on_enter_TakeOff(self):
         file.write("\non_enter_TakeOff")
+        text = "\n -> DroneSM::takeoffCall." + str(self.heightTg)
+        trace.write(text)
         print("Taking off!")
-        self.vehicle.simple_takeoff(self.aTargetAltitude)  # Take off to target altitude
+        self.vehicle.simple_takeoff(self.heightTg)  # Take off to target altitude
         time.sleep(3)
+        trace.write("\n -> tock -> tock -> tock")
+
 
     def on_enter_Mission(self):
         file.write("\non_enter_Mission")
         print("Going towards first point for 30 seconds ...")
-        point1 = LocationGlobalRelative(-35.361354, 149.165218, self.aTargetAltitude)
+        point1 = LocationGlobalRelative(-35.361354, 149.165218, self.heightTg)
+        text = "\nGoing to " + str(point1.lat) + "; "+ str(point1.lon) + "; " + str(point1.alt) 
+        file.write(text)
+        trace.write(" -> DroneSM::gotoCall.1.1.1 ")
+
         self.vehicle.simple_goto(point1, 0, 120)
 
         # sleep so we can see the change in map
         time.sleep(30)
+        trace.write(" -> tock ")
+
 
         print("Going towards second point for 30 seconds ...")
-        point2 = LocationGlobalRelative(-35.363244, 149.168801, self.aTargetAltitude)
+        point2 = LocationGlobalRelative(-35.363244, 149.168801, self.heightTg)
         self.vehicle.simple_goto(point2, groundspeed=120, airspeed=0)
+        text = "\nGoing to " + str(point2.lat) + "; "+ str(point2.lon) + "; " + str(point2.alt) 
+        file.write(text)
+        trace.write(" -> DroneSM::gotoCall.2.2.2 ")
 
         time.sleep(30)
+        trace.write(" -> tock ")
+
 
     def on_enter_Final(self):
         file.write("\non_enter_Final")
         # Close vehicle object before exiting script
         print("Close vehicle object")
+        trace.write(" -> DroneSM::terminate ")
+
         self.vehicle.close()
 
 
     def run(self):
         file.write("\n#########run#########")
+        trace.write(" DroneSM = ")
+
         cont = 0
         while True:
             if(self.state == 'Initial'):
@@ -213,12 +233,16 @@ class DroneSM(GraphMachine):
             
             if(cont == 0):
                 file.write("\n-> tock")
+                trace.write(" -> tock")
                 time.sleep(5)
             else:
                 cont = 0           
 
+            self.start = random.choice([True, False])
+
 ######################## Instantiating and Running the State Machine #######################  
-file = open("Pytransitions/DroneSM/Data/trace.txt", "w")
+file = open("Pytransitions/DroneSM/Data/log.txt", "w")
+trace = open("Pytransitions/DroneSM/Data/trace.txt", "w")
 machine = DroneSM()
 machine.run()
 file.close()
